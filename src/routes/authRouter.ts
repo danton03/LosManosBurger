5d671abccb2ef66e35a88a990b48da9ef68f1e33
Danton Matheus <dantoncosta03@hotmail.com>
2022-09-27 23:56:49 -0300

    :sparkles: feat: cria o router de autenticação

---
+++ src/routes/authRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+// import { createUser, login } from "../controllers/authController";
+// import { validateSchemaMiddleware } from "../middlewares/validateSchema";
+// import { loginSchema, signUpSchema } from "../schemas/authSchema";
+
+const authRouter = Router();
+
+//authRouter.post('/login', validateSchemaMiddleware(loginSchema), login);
+//authRouter.post('/signup', validateSchemaMiddleware(signUpSchema), createUser);
+
+export default authRouter;
