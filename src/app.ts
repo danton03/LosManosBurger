0edec81b886439725f58665e8c44dcd116f484e8
Danton Matheus <dantoncosta03@hotmail.com>
2022-09-27 23:40:09 -0300

    :package: build: Instala as dependências para a feat de autenticação

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express, { json } from 'express';
 import 'express-async-errors';
 
-//import handleErrorsMiddleware from './middlewares/errorMiddleware';
+import handleErrorsMiddleware from './middlewares/errorMiddleware';
 //import router from './routes/router';
 
 dotenv.config();
@@ -10,6 +10,6 @@ dotenv.config();
 const app = express();
 app.use(json());
 //app.use(router);
-//app.use(handleErrorsMiddleware);
+app.use(handleErrorsMiddleware);
 
 export default app;
