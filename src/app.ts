200a2c7ee04c147044073e0a4f553340fd04472d
Danton Matheus <dantoncosta03@hotmail.com>
2022-09-27 23:54:57 -0300

    :sparkles: feat: cria o arquivo do router

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import express, { json } from 'express';
 import 'express-async-errors';
 
 import handleErrorsMiddleware from './middlewares/errorMiddleware';
-//import router from './routes/router';
+import router from './routes/router';
 
 dotenv.config();
 
 const app = express();
 app.use(json());
-//app.use(router);
+app.use(router);
 app.use(handleErrorsMiddleware);
 
 export default app;
