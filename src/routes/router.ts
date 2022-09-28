6111c807b51331dbc02d1d2e2e63d09469bb8356
Danton Matheus <dantoncosta03@hotmail.com>
2022-09-28 00:05:06 -0300

    :sparkles: feat: cria a rota de cadastro do usuário

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { ensureAuthenticatedMiddleware } from '../middlewares/authMiddleware';
-//import authRouter from './authRouter';
+import authRouter from './authRouter';
 
 const router = Router();
-//router.use(authRouter);
+router.use(authRouter);
 router.use(ensureAuthenticatedMiddleware);
 
 export default router;
