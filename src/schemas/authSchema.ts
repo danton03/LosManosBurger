26c3fc8e427c5fadea7685480539c9f3e7c772ff
Danton Matheus <37675973+danton03@users.noreply.github.com>
2022-09-29 16:07:14 -0300

    :sparkles: feat: altera o schema de signup para receber o nome do usuário

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,6 +1,7 @@
 import joi from 'joi';
 
 export const signUpSchema = joi.object({
+  name: joi.string().trim().required(),
   email: joi.string().email().required(), 
   password: joi.string().trim().required(),
   confirmPassword: joi.string().valid(joi.ref('password')).required(),
