8301f73b17a92bd67711ab3cdce6cd7528a6ad2f
Danton Matheus <37675973+danton03@users.noreply.github.com>
2022-10-02 17:06:53 -0300

    :sparkles: feat: cria a rota que retorna os locais de entrega e endereços do usuário

--- src/routes/deliveryRouter.ts
+++ src/routes/deliveryRouter.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { getAllDeliveryLocations } from "../controllers/deliveryController";
+import { 
+	getAllDeliveryLocations, 
+	getDeliveryLocationsAndUserAddresses 
+} from "../controllers/deliveryController";
+import { ensureAuthenticatedMiddleware } from "../middlewares/authMiddleware";
 
 const deliveryRouter = Router();
 
 deliveryRouter.get("/delivery", getAllDeliveryLocations);
+deliveryRouter.use(ensureAuthenticatedMiddleware);
+deliveryRouter.get("/delivery/clients", getDeliveryLocationsAndUserAddresses);
 
 export default deliveryRouter;
