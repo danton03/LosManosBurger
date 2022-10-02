8fd39ce7a31b1f6fa92b4e38b3bd9efb896afb82
Danton Matheus <37675973+danton03@users.noreply.github.com>
2022-10-02 18:20:16 -0300

    :sparkles: feat: cria o repository que retorna os endereços do usuário e locais de entrega

--- src/repositories/deliveryRepository.ts
+++ src/repositories/deliveryRepository.ts
@@ -2,7 +2,7 @@ import { prisma } from "../config/database";
 //eslint-disable prisma
 
 async function getDeliveryLocations() {
-	const districts = await prisma.cities.findMany({
+	const deliveryLocations = await prisma.cities.findMany({
 		where: {},
 		include: {
 			Districts: {
@@ -19,9 +19,48 @@ async function getDeliveryLocations() {
 		}
 	});
   
-	return districts;
+	return deliveryLocations;
+}
+
+async function getDeliveryLocationsAndUserAddresses(userId: number) {
+	const deliveryLocations = await prisma.cities.findMany({
+		where: {},
+		include: {
+			Districts: {
+				select: {
+					id: true,
+					name: true,
+					Delivery: {
+						select: {
+							price: true
+						}
+					}
+				},
+			},
+		}
+	});
+  
+	const userAddresses = await prisma.$queryRaw`
+		SELECT addresses.id, addresses.street, addresses."houseNumber", districts.name AS district, cities.name AS city, states.name AS "state"
+		FROM addresses
+		JOIN districts
+		ON districts.id = addresses."districtId"
+		JOIN cities
+		ON cities.id = districts."cityId"
+		JOIN states
+		ON states.id = cities."stateId"
+		WHERE addresses.id = ${userId};
+	`;
+
+	const deliveryLocationsWithUserAddresses = {
+		userAddresses,
+		...deliveryLocations
+	};
+
+	return deliveryLocationsWithUserAddresses;
 }
 
 export const deliveryRepository = {
-	getDeliveryLocations
+	getDeliveryLocations,
+	getDeliveryLocationsAndUserAddresses
 };
