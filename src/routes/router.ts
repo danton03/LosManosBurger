f1fa887b1e06a7a6e63f78810f085cd284971fba
Danton Matheus <37675973+danton03@users.noreply.github.com>
2022-10-02 01:55:20 -0300

    :sparkles: feat: adiciona a rota de delivery no router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRouter from "./authRouter";
+import deliveryRouter from "./deliveryRouter";
 import productsRouter from "./productsRouter";
 
 const router = Router();
 router.use(authRouter);
 router.use(productsRouter);
+router.use(deliveryRouter);
 
 export default router;
