0ff320ee747d8234f98a2feb1c74b83069907bb1
Danton Matheus <37675973+danton03@users.noreply.github.com>
2022-10-08 02:08:26 -0300

    :bug: fix: corrige o erro de acesso negado por falta do cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import express, { json } from "express";
 import "express-async-errors";
-
+import cors from "cors";
 import handleErrorsMiddleware from "./middlewares/errorMiddleware";
 import router from "./routes/router";
 
@@ -9,6 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(json());
+app.use(cors());
 app.use(router);
 app.use(handleErrorsMiddleware);
 
